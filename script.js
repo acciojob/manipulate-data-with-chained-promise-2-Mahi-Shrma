f28d1bfaf6a88fe6c1b4eaca09147b60c6477ff0
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2024-11-07 14:15:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+const output = document.getElementById('output');
+
+function f1() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve([1, 2, 3, 4]);
+        }, 3000);
+    });
+}
+
+f1()
+    .then((numbers) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const result = numbers.filter((num) => num % 2 === 0);
+                output.innerText = result.join(', ');
+                resolve(result);
+            }, 1000);
+        });
+    })
+    .then((numbers) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                const result = numbers.map((num) => num * 2);
+                output.innerText = result.join(', ');
+                resolve(result);
+            }, 2000);
+        });
+    })
+    .catch((error) => console.error(error));
+
+
+
+
 //your JS code here. If required.
 // const output = document.getElementById('output');
 
@@ -29,36 +63,36 @@
 //         });
 //     })
 //     .catch((error) => console.error(error));
-const output = document.getElementById('output');
+// const output = document.getElementById('output');
 
-function f1() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve([1, 2, 3, 4]);
-        }, 3000);
-    });
-}
+// function f1() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve([1, 2, 3, 4]);
+//         }, 3000);
+//     });
+// }
 
-f1()
-    .then((numbers) => {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                const result = numbers.filter((num) => num % 2 === 0);
-                output.innerText = result.join(', ');
-                resolve(result);
-            }, 1000);
-        });
-    })
-    .then((numbers) => {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                const result = numbers.map((num) => num * 2);
-                output.innerText = result.join(', ');
-                resolve(result);
-            }, 2000);
-        });
-    })
-    .catch((error) => console.error(error));
+// f1()
+//     .then((numbers) => {
+//         return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 const result = numbers.filter((num) => num % 2 === 0);
+//                 output.innerText = result.join(', ');
+//                 resolve(result);
+//             }, 1000);
+//         });
+//     })
+//     .then((numbers) => {
+//         return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 const result = numbers.map((num) => num * 2);
+//                 output.innerText = result.join(', ');
+//                 resolve(result);
+//             }, 2000);
+//         });
+//     })
+//     .catch((error) => console.error(error));
 
 // const output= document.getElementById('output');
 
