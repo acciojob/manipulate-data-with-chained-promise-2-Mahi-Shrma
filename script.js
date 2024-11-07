1be0e9f3d81a3278be45818a03524f501ee32dfc
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2024-11-07 13:46:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 //your JS code here. If required.
+// const output = document.getElementById('output');
+
+// function f1() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve([1, 2, 3, 4]);
+//         }, 3000);
+//     });
+// }
+
+// f1()
+//     .then((numbers) => {
+//         return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 const result = numbers.filter((num) => num % 2 === 0);
+//                 output.innerText = result.join(', ');
+//                 resolve(result);
+//             }, 1000);
+//         });
+//     })
+//     .then((numbers) => {
+//         return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 const result = numbers.map((num) => num * 2);
+//                 output.innerText = result.join(', ');
+//                 resolve(result);
+//             }, 2000);
+//         });
+//     })
+//     .catch((error) => console.error(error));
+
 const output= document.getElementById('output');
 
 function f1(){
